b970a250d3aa9fc39b8632a2df9d8ebbb5e7359b
Amplify <not.amplify.x@gmail.com>
2024-11-04 20:16:21 -0500

    comments

--- public/static/assets/js/browser/functions.js
+++ public/static/assets/js/browser/functions.js
@@ -182,125 +182,93 @@ class Functions {
 
   extrasMenu(button) {
     let content = this.ui.createElement("div", {}, [
-      this.ui.createElement("div", { class: "menu-item", id: "openNewTab" }, [
+      //New Tab
+      this.ui.createElement("div", { class: "menu-item", id: "openNewTab", onclick: ()=>{
+        this.tabs.createTab("daydream://newtab");
+      } }, [
         this.ui.createElement("span", { class: "material-symbols-outlined" }, ["tab"]),
         this.ui.createElement("span", { class: "menu-label" }, ["Open New Tab"]),
         this.ui.createElement("span", { class: "menu-key" }, ["Alt + T"])
       ]),
-      this.ui.createElement("div", { class: "menu-item", id: "openNewWindow" }, [
+      //New Window
+      this.ui.createElement("div", { class: "menu-item", id: "openNewWindow", onclick: ()=>{
+        this.tabs.createTab("daydream://newtab");
+      }  }, [
         this.ui.createElement("span", { class: "material-symbols-outlined" }, ["open_in_new"]),
         this.ui.createElement("span", { class: "menu-label" }, ["Open a New Window"]),
         this.ui.createElement("span", { class: "menu-key" }, ["Alt + N"])
       ]),
-      this.ui.createElement("div", { class: "menu-item", id: "openNewABWindow" }, [
+      //New Incognito Window
+      this.ui.createElement("div", { class: "menu-item", id: "openNewABWindow", onclick: ()=>{
+        this.tabs.createTab("daydream://newtab");
+      }  }, [
         this.ui.createElement("span", { class: "material-symbols-outlined" }, ["visibility_off"]),
         this.ui.createElement("span", { class: "menu-label" }, ["Open an Incognito Window"]),
         this.ui.createElement("span", { class: "menu-key" }, ["Alt + Shift + N"])
       ]),
       this.ui.createElement("div", { class: "menu-row" }, [
         this.ui.createElement("span", { style: "margin: 0px 20px;" }, ["Zoom"]),
-        this.ui.createElement("div", { class: "menu-item", id: "zoom-out" }, [
+        // Zoom Out
+        this.ui.createElement("div", { class: "menu-item", id: "zoom-out", onclick: ()=>{
+          this.tabs.createTab("daydream://newtab");
+        }  }, [
           this.ui.createElement("span", { class: "material-symbols-outlined" }, ["remove"]),
         ]),
         this.ui.createElement("span", { class: "menu-label", id: "zoom-percentage" }, ["100%"]),
-        this.ui.createElement("div", { class: "menu-item", id: "zoom-in" }, [
+        //Zoom In
+        this.ui.createElement("div", { class: "menu-item", id: "zoom-in", onclick: ()=>{
+          this.tabs.createTab("daydream://newtab");
+        }  }, [
           this.ui.createElement("span", { class: "material-symbols-outlined" }, ["add"])
         ]),
-        this.ui.createElement("div", { class: "menu-item", id: "fullscreen" }, [
+        // Fullscreen
+        this.ui.createElement("div", { class: "menu-item", id: "fullscreen", onclick: ()=>{
+          this.tabs.createTab("daydream://newtab");
+        } }, [
           this.ui.createElement("span", { class: "material-symbols-outlined" }, ["open_in_full"])
         ]),
       ]),
-      this.ui.createElement("div", { class: "menu-item", id: "openBookmarks" }, [
+      // Bookmarks
+      this.ui.createElement("div", { class: "menu-item", id: "openBookmarks", onclick: ()=>{
+        this.tabs.createTab("daydream://newtab");
+      }  }, [
         this.ui.createElement("span", { class: "material-symbols-outlined" }, ["hotel_class"]),
         this.ui.createElement("span", { class: "menu-label" }, ["Bookmarks"]),
         this.ui.createElement("span", { class: "menu-key" }, ["Alt + Shift + B"])
       ]),
-      this.ui.createElement("div", { class: "menu-item", id: "openGames" }, [
+      // Games
+      this.ui.createElement("div", { class: "menu-item", id: "openGames", onclick: ()=>{
+        this.tabs.createTab("daydream://newtab");
+      }  }, [
         this.ui.createElement("span", { class: "material-symbols-outlined" }, ["playing_cards"]),
         this.ui.createElement("span", { class: "menu-label" }, ["Games"]),
         this.ui.createElement("span", { class: "menu-key" }, ["Alt + Shift + G"])
       ]),
-      this.ui.createElement("div", { class: "menu-item", id: "openExtensions" }, [
+      // Extensions
+      this.ui.createElement("div", { class: "menu-item", id: "openExtensions", onclick: ()=>{
+        this.tabs.createTab("daydream://newtab");
+      }  }, [
         this.ui.createElement("span", { class: "material-symbols-outlined" }, ["extension"]),
         this.ui.createElement("span", { class: "menu-label" }, ["Extensions"]),
         this.ui.createElement("span", { class: "menu-key" }, ["Alt + Shift + E"])
-      ]),
-      this.ui.createElement("div", { class: "menu-item", id: "screenshot" }, [
+      ]), 
+      // Screenshot
+      this.ui.createElement("div", { class: "menu-item", id: "screenshot", onclick: ()=>{
+        this.tabs.createTab("daydream://newtab");
+      }  }, [
         this.ui.createElement("span", { class: "material-symbols-outlined" }, ["screenshot"]),
         this.ui.createElement("span", { class: "menu-label" }, ["Screenshot"]),
         this.ui.createElement("span", { class: "menu-key" }, ["Alt + C"])
       ]),
-      this.ui.createElement("div", { class: "menu-item", id: "inspectElement" }, [
+      //Inspect Element
+      this.ui.createElement("div", { class: "menu-item", id: "inspectElement", onclick: ()=>{
+        this.tabs.createTab("daydream://newtab");
+      }  }, [
         this.ui.createElement("span", { class: "material-symbols-outlined" }, ["code"]),
         this.ui.createElement("span", { class: "menu-label" }, ["Inspect Element"]),
         this.ui.createElement("span", { class: "menu-key" }, ["Alt + Shift + I"])
       ]),
     ])
     this.nightmarePlugins.sidemenu.attachTo(button, content);
-    setTimeout(() => {
-      this.extrasMenuFunctions();
-    }, 50);
   }
-
-  extrasMenuFunctions() {
-    if (document.querySelector(".menu-container") != null) {
-      console.log("Menu container found");
-      const openNewTab = document.getElementById("openNewTab");
-      const openNewWindow = document.getElementById("openNewWindow");
-      const openNewABWindow = document.getElementById("openNewABWindow");
-      const zoomOut = document.getElementById("zoom-out");
-      const zoomIn = document.getElementById("zoom-in");
-      const fullscreen = document.getElementById("fullscreen");
-      const openBookmarks = document.getElementById("openBookmarks");
-      const openGames = document.getElementById("openGames");
-      const openExtensions = document.getElementById("openExtensions");
-      const screenshot = document.getElementById("screenshot");
-      const inspectElement = document.getElementById("inspectElement");
-
-      openNewTab.addEventListener("click", () => {
-        this.tabs.createTab("daydream://newtab");
-      });
-
-      openNewWindow.addEventListener("click", () => {
-        console.log("Opening new window");
-      });
-
-      openNewABWindow.addEventListener("click", () => {
-        console.log("Opening new incognito window");
-      });
-
-      zoomOut.addEventListener("click", () => {
-        console.log("Zooming out");
-      });
-
-      zoomIn.addEventListener("click", () => {
-        console.log("Zooming in");
-      });
-
-      fullscreen.addEventListener("click", () => {
-        console.log("Fullscreen");
-      });
-
-      openBookmarks.addEventListener("click", () => {
-        console.log("Opening bookmarks");
-      });
-
-      openGames.addEventListener("click", () => {
-        console.log("Opening games");
-      });
-
-      openExtensions.addEventListener("click", () => {
-        console.log("Opening extensions");
-      });
-
-      screenshot.addEventListener("click", () => {
-        console.log("Taking screenshot");
-      });
-
-      inspectElement.addEventListener("click", () => {
-        this.inspectElement();
-      });
-    }
-  }
-
 }
