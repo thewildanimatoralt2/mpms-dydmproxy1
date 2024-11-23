706c7f7a60a64138f7f9af91e9f8d957dd8345e3
not-amplify <not.amplify.x@gmail.com>
2024-11-22 21:10:44 -0500

    working out RH

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
 import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
 import { server as wisp } from "@mercuryworkshop/wisp-js/server";
+import {
+  createRammerhead,
+  shouldRouteRh,
+  routeRhUpgrade,
+  routeRhRequest,
+} from "./src/RH/index.js";
 
 const server = http.createServer();
 const app = express();
@@ -26,18 +32,43 @@ app.use("/@/", express.static(uvPath));
 app.use("/libcurl/", express.static(libcurlPath));
 app.use("/baremux/", express.static(baremuxPath));
 
+const rh = createRammerhead({
+  logLevel: 'info',
+  reverseProxy: false,
+  disableLocalStorageSync: false,
+  disableHttp2: false,
+})
+
 app.use("/", routes);
 
 wisp.options.dns_servers = ["1.1.1.3", "1.0.0.3"];
 
 server.on("request", (req, res) => {
-  app(req, res);
+  const url = new URL(req.url);
+  console.log(url.host);
+  console.log(url.hostname)
+  if (shouldRouteRh(req)) {
+    routeRhRequest(
+      rh,
+      req,
+      res
+    );
+  } else {
+    app(req, res);
+  }
 });
 
 server.on("upgrade", (req, socket, head) => {
   if (req.url.endsWith("/wisp/")) {
     wisp.routeRequest(req, socket, head);
     console.log("Wisp upgrade request:" + req);
+  } else if (shouldRouteRh(req)) {
+    routeRhUpgrade(
+      rh,
+      req,
+      socket,
+      head
+    );
   }
 });
 
@@ -56,20 +87,20 @@ server.on("listening", () => {
 ██████╔╝██║  ██║   ██║   ██████╔╝██║  ██║███████╗██║  ██║██║ ╚═╝ ██║    ██╔╝ ██╗
 ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚═╝  ╚═╝
                                                                                 
-`),
-    ),
+`)
+    )
   );
   console.log(
-    `  ${chalk.bold(host("Local System:"))}            http://${address.family === "IPv6" ? `[${address.address}]` : address.address}${address.port === 80 ? "" : ":" + chalk.bold(address.port)}`,
+    `  ${chalk.bold(host("Local System:"))}            http://${address.family === "IPv6" ? `[${address.address}]` : address.address}${address.port === 80 ? "" : ":" + chalk.bold(address.port)}`
   );
 
   console.log(
-    `  ${chalk.bold(host("Local System:"))}            http://localhost${address.port === 8080 ? "" : ":" + chalk.bold(address.port)}`,
+    `  ${chalk.bold(host("Local System:"))}            http://localhost${address.port === 8080 ? "" : ":" + chalk.bold(address.port)}`
   );
 
   try {
     console.log(
-      `  ${chalk.bold(host("On Your Network:"))}  http://${hostname()}${address.port === 8080 ? "" : ":" + chalk.bold(address.port)}`,
+      `  ${chalk.bold(host("On Your Network:"))}  http://${hostname()}${address.port === 8080 ? "" : ":" + chalk.bold(address.port)}`
     );
   } catch (err) {
     // can't find LAN interface
@@ -77,13 +108,13 @@ server.on("listening", () => {
 
   if (process.env.REPL_SLUG && process.env.REPL_OWNER) {
     console.log(
-      `  ${chalk.bold(host("Replit:"))}           https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`,
+      `  ${chalk.bold(host("Replit:"))}           https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
     );
   }
 
   if (process.env.HOSTNAME && process.env.GITPOD_WORKSPACE_CLUSTER_HOST) {
     console.log(
-      `  ${chalk.bold(host("Gitpod:"))}           https://${PORT}-${process.env.HOSTNAME}.${process.env.GITPOD_WORKSPACE_CLUSTER_HOST}`,
+      `  ${chalk.bold(host("Gitpod:"))}           https://${PORT}-${process.env.HOSTNAME}.${process.env.GITPOD_WORKSPACE_CLUSTER_HOST}`
     );
   }
 
@@ -92,7 +123,7 @@ server.on("listening", () => {
     process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN
   ) {
     console.log(
-      `  ${chalk.bold(host("Github Codespaces:"))}           https://${process.env.CODESPACE_NAME}-${address.port === 80 ? "" : address.port}.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`,
+      `  ${chalk.bold(host("Github Codespaces:"))}           https://${process.env.CODESPACE_NAME}-${address.port === 80 ? "" : address.port}.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`
     );
   }
 });
